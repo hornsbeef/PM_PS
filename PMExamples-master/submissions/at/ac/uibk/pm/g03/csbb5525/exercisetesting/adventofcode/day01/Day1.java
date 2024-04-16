6108aaa98e69e6b8473e34bda4540b333223b848
hornsbeef <login.horninger@gmail.com>
2024-04-16 17:31:28 +0200

    small updates for day1

--- PMExamples-master/submissions/at/ac/uibk/pm/g03/csbb5525/exercisetesting/adventofcode/day01/Day1.java
+++ PMExamples-master/submissions/at/ac/uibk/pm/g03/csbb5525/exercisetesting/adventofcode/day01/Day1.java
@@ -13,6 +13,15 @@ public class Day1 {
 
     public static void main(String[] args){
 
+        /*
+        the calibration value can be found by combining
+        the first digit and the last digit (in that order)
+        to form a single two-digit number.
+        Then adding all the numbers from all lines together
+
+         */
+
+
         Scanner scan = null;
         File data = new File ("submissions/at/ac/uibk/pm/g03/csbb5525/exercisetesting/adventofcode/day01/input");
 
@@ -25,32 +34,52 @@ public class Day1 {
         }
 
 
-        List<ArrayList> list = new ArrayList<>();
+        //List<ArrayList> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
 
         //while(scan.hasNext()){
-            //testing:
-            //System.out.println(scan.nextLine());
-
             String temp = scan.nextLine();
-
 
             List<Character> characterList = temp.chars().mapToObj(c-> (char) c).toList();
 
-            characterList.forEach(System.out::print);
-            System.out.println();
-
-           //List<Integer> integerList =
+            List<Character> charactersListdigits =
                    characterList.stream()
-                                        .filter(c -> Character.isDigit(c)).forEach(System.out::print);
-                                        //.mapToInt( i -> (int) i)
-                                        //.boxed().collect(Collectors.toList());
+                                .filter(c -> Character.isDigit(c))
+                                .toList();
 
-           //integerList.forEach(System.out::println);
+                                //.forEach( (c) -> {
+                                //    System.out.println(c);
+                                //    //System.out.println(c.getClass());
+                                //});
+                                //.mapToInt( i -> (int) i)
+                                //.boxed().collect(Collectors.toList());
+
+                                //.map( c -> (int) c)//todo: something goes wrong here!
+                                //.toList();
+
+
+            //integerList.forEach(System.out::println);
 
 
                             //.mapToInt(i -> (int) i)
                             //.collect(Collectors.toList());
+
+            charactersListdigits.forEach( i -> {
+                System.out.println(i);
+                System.out.println(i.getClass());
+            });
+
+            int count = charactersListdigits.size();
+
+            switch(count) {
+                case 0 -> System.out.println("need to skip line -> use continue?");//continue;
+                case 1 -> list.addAll(charactersListdigits.stream().map(i->(int)i).toList());
+                default -> {
+                    Integer first = Integer.valueOf(charactersListdigits.get(0));
+                    //Integer last = Integer.valueOf(charactersListdigits);
+                }
+            }
 
 
         //}
