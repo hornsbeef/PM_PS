013a16d11f71d523536cc9fe2f6374328511f19e
hornsbeef <login.horninger@gmail.com>
2024-04-19 10:38:22 +0200

    reordered package structure

--- PMExamples-master/submissions/at/ac/uibk/pm/g03/csbb5525/exercisetesting/proseminar/s01e01.java
+++ PMExamples-master/submissions/at/ac/uibk/pm/g03/csbb5525/exercisetesting/proseminar/s01e01.java
@@ -1,4 +1,4 @@
-package at.ac.uibk.pm.g03.csbb5525.exercisetesting;
+package at.ac.uibk.pm.g03.csbb5525.exercisetesting.proseminar;
 
 public class s01e01 {
     public static void main(String[] args) {
